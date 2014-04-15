f7dd86bcbf6923f86ce291c417c32fd7ffcb4a82
Paula Gray-Overtoom <pgrayove@gmail.com>
2014-04-15 09:29:35 -0400

    added mimemail, revisioning, entity and email updates

--- sites/all/themes/corolla/template.php
+++ sites/all/themes/corolla/template.php
@@ -131,3 +131,16 @@ function corolla_fieldset($vars) {
 
   return $output;
 }
+
+/**
+ * Process variables to format email messages.
+ *
+ * @see htmlmail.tpl.php
+ */
+function corolla_preprocess_htmlmail(&$variables) {
+  // Run the default preprocess function.
+  template_preprocess_htmlmail($variables);
+
+  // Use the 'HTML Email' text format for the message body.
+  $variables['body'] = check_markup($variables['body'], 'html_email');
+}
